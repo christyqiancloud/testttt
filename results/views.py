ddc00a8c15518b55c5b50e7e27153b37a00505e7
christinaqian <christinaqian@outlook.com>
2022-10-06 00:37:38 +0000

    hardcode links in for usatest

--- results/views.py
+++ results/views.py
@@ -7,19 +7,20 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 import sys
-sys.path.append("mysite/polls/views.py")
+
 # def current_datetime(request):
 #     now = datetime.datetime.now()
 #     html = "<html><body>It is now %s.</body></html>" % now
 #     return HttpResponse(html)
 
 def correct(request):
-    html = "<html><body>You are correct!\n<a href='http://localhost:8000/polls/'>Back to polls page?</a></body></html>"
+    html = "<html><body>You are correct!\n<a href='http://localhost:8000/usatest/'>Back to usatest page?</a><br><br><a href='http://localhost:8000/usatest/1'>See Other Results/Go Back To Poll?</a></body></html>"
     return HttpResponse(html)
 
 def incorrect(request):
-    html = f"<html><body>You are wrong!<a href=http://localhost:8000/polls/1>Try again? </a><br><br><br><a href=http://localhost:8000/results/answer>See answers?</a></body></html>"
+    html = f"<html><body>You are wrong!<a href=http://localhost:8000/usatest/1>Try again? </a><br><br><a href=http://localhost:8000/results/answer>See answers?</a></body></html>"
     return HttpResponse(html)
 def answer(request):
-    html = f"<html><body>Answer is: Salt Lake City<a href=http://localhost:8000/polls/1> Go back to poll?</a></body></html>"
+    html = f"<html><body>Answer is: Salt Lake City<a href=http://localhost:8000/usatest/1> Go back to poll?</a></body></html>"
     return HttpResponse(html)
+
